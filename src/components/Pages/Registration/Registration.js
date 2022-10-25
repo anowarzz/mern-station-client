c7af5813715b8694e9fcea5fc1f3147f44f095c2
anowarzz <anowarhosen444@gmail.com>
2022-10-25 20:49:16 +0600

    project initialized

---
+++ src/components/Pages/Registration/Registration.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Registration = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">This is the registratin section</h2>
+        </div>
+    );
+};
+
+export default Registration;
