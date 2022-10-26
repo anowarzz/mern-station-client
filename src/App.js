7eae013407d8dd4f2f4298a1269ad3750eaa2778
anowarzz <anowarhosen444@gmail.com>
2022-10-26 12:03:17 +0600

    added navbar again

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import { routes } from './Routes/AllRoutes/Routes';
+import { Toaster } from 'react-hot-toast';
 
 
 function App() {
   return (
    <div>
     <RouterProvider router = {routes}>
-      
     </RouterProvider>
+    <Toaster /> 
    </div>
   );
 }
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,55 +1,45 @@
-import { Avatar, Dropdown, Navbar } from 'flowbite-react';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from '../../../assets/mern-logo.png'
 
-
+import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Navbar } from "flowbite-react";
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../../assets/mern-logo.png";
 
 const Header = () => {
-    return (
-   
-<Navbar className='bg-gray-200'
-  fluid={true}
-  rounded={true}
->
-  <Navbar.Brand >
-    <img
-      src={logo}
-      className="mr-3 md:h-20 sm:h-10  w-20"
-      alt="Mern Stack Logo"
-    />
-    <span className="self-center whitespace-nowrap sm:text-3xl font-oswald text-blue-600 text-xl">
-      <Link to='/'> 
-      Mern Station</Link>
-    </span>
-  </Navbar.Brand>
-  <div className="flex md:order-2">
-    
-    <Navbar.Toggle />
-  </div>
-  <Navbar.Collapse>
-    <Navbar.Link
-      href="/navbars"
-      active={true}
-    >
-      Home
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      About
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      Services
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      Pricing
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      Contact
-    </Navbar.Link>
-  </Navbar.Collapse>
-</Navbar>
+  return (
+    <Navbar className="bg-gray-200"  fluid={true} rounded={true}>
 
-    );
+      <Navbar.Brand>
+        <img
+          src={logo}
+          className="mr-3 md:h-20 sm:h-10  w-20"
+          alt="Mern Stack Logo"
+        />
+        <span className="self-center whitespace-nowrap sm:text-3xl font-oswald text-blue-600 text-xl">
+          <Link to="/">Mern Station</Link>
+        </span>
+      </Navbar.Brand>
+
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+
+        <NavLink className='text-lg' to = "/courses"> Courses </NavLink>
+
+        <NavLink className='text-lg' to="faq"> FAQ </NavLink>
+
+        <NavLink className='text-lg' to ='blog'> Blog </NavLink>
+
+        <NavLink className='text-lg' to>
+         <p className="border border-dotted border-black px-2"> Dark <FontAwesomeIcon  icon={faMoon}/></p>
+           </NavLink>
+
+        <Navbar.Link to="/login">
+          <button className="bg-black text-white text-lg "></button>
+        </Navbar.Link>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Header;
