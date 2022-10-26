d6b3bfbc319ae922861be636e1cf4fa8517e99e2
anowarzz <anowarhosen444@gmail.com>
2022-10-26 10:09:24 +0600

    added nav logo

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h3 className="text-3xl">This is the home section where it all started</h3>
+            <h3 className="text-3xl font-oswald ">This is the home section where it all started</h3>
         </div>
     );
 };
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,8 +1,54 @@
+import { Avatar, Dropdown, Navbar } from 'flowbite-react';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../assets/mern-logo.png'
+
+
 
 const Header = () => {
     return (
-      <h3>This is the header</h3>
+   
+<Navbar className='bg-gray-200'
+  fluid={true}
+  rounded={true}
+>
+  <Navbar.Brand >
+    <img
+      src={logo}
+      className="mr-3 md:h-20 sm:h-10  w-20"
+      alt="Mern Stack Logo"
+    />
+    <span className="self-center whitespace-nowrap sm:text-3xl font-oswald text-blue-600 text-xl">
+      <Link to='/'> 
+      Mern Station</Link>
+    </span>
+  </Navbar.Brand>
+  <div className="flex md:order-2">
+    
+    <Navbar.Toggle />
+  </div>
+  <Navbar.Collapse>
+    <Navbar.Link
+      href="/navbars"
+      active={true}
+    >
+      Home
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      About
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Services
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Pricing
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Contact
+    </Navbar.Link>
+  </Navbar.Collapse>
+</Navbar>
+
     );
 };
 
