183f922c8dfea94a6b517f0433091be2c69c54c2
anowarzz <anowarhosen444@gmail.com>
2022-10-26 12:46:43 +0600

    navbar darkmode icon added

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,45 +1,68 @@
-
-import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import { faMoon, faSun} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Navbar } from "flowbite-react";
 import React from "react";
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../../assets/mern-logo.png";
 
+
+
 const Header = () => {
-  return (
-    <Navbar className="bg-gray-200"  fluid={true} rounded={true}>
+  
+const [dark, setDark] = useState(false);
 
-      <Navbar.Brand>
-        <img
-          src={logo}
-          className="mr-3 md:h-20 sm:h-10  w-20"
-          alt="Mern Stack Logo"
-        />
-        <span className="self-center whitespace-nowrap sm:text-3xl font-oswald text-blue-600 text-xl">
-          <Link to="/">Mern Station</Link>
-        </span>
-      </Navbar.Brand>
+const handleTheme = () => {
+  setDark(!dark)
+}
 
-      <Navbar.Toggle />
-      <Navbar.Collapse>
+    return (
 
-        <NavLink className='text-lg' to = "/courses"> Courses </NavLink>
 
-        <NavLink className='text-lg' to="faq"> FAQ </NavLink>
+<Navbar className="bg-zinc-200"  fluid={true} rounded={true}>
 
-        <NavLink className='text-lg' to ='blog'> Blog </NavLink>
+<Navbar.Brand>
+  <img
+    src={logo}
+    className="mr-3 md:h-20 sm:h-10  w-20"
+    alt="Mern Stack Logo"
+  />
+  <span className="self-center whitespace-nowrap sm:text-3xl font-oswald text-blue-600 text-xl">
+    <Link to="/">Mern Station</Link>
+  </span>
+  </Navbar.Brand>
 
-        <NavLink className='text-lg' to>
-         <p className="border border-dotted border-black px-2"> Dark <FontAwesomeIcon  icon={faMoon}/></p>
-           </NavLink>
+  <Navbar.Toggle />
+  <Navbar.Collapse>
 
-        <Navbar.Link to="/login">
-          <button className="bg-black text-white text-lg "></button>
-        </Navbar.Link>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+
+  
+ <NavLink className='text-lg py-1' to = "/courses"> Courses </NavLink>
+
+ <NavLink className='text-lg py-1' to="faq"> FAQ </NavLink>
+
+  <NavLink className='text-lg py-1' to ='blog'> Blog </NavLink>
+
+  <NavLink onClick={handleTheme}  className='text-lg py-1 border border-dotted border-black  px-2 '>
+  {
+    dark ? <p > Light <FontAwesomeIcon icon={faSun} /> </p>
+    : <p className=""> Dark <FontAwesomeIcon  icon={faMoon}/></p>
+    
+    
+  } 
+  </NavLink>
+
+ <NavLink to="/login" className='text-lg'>
+  <button className="bg-black text-white py-1 px-6">
+  Login
+ </button>
+  </NavLink>
+
+</Navbar.Collapse>
+</Navbar>
+
+
+    )
 };
 
 export default Header;
