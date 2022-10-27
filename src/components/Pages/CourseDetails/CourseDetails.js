121d1a3a28a9ff492fa9586f85fe86746355211f
anowarzz <anowarhosen444@gmail.com>
2022-10-27 22:25:25 +0600

    loaded api photos

--- src/components/Pages/CourseDetails/CourseDetails.js
+++ src/components/Pages/CourseDetails/CourseDetails.js
@@ -6,7 +6,19 @@ const CourseDetails = ({course}) => {
 
   return (
     <div>
-      <h3>Here goes all the cards {course.length}</h3>
+
+    
+<div className="sm:max-w-auto rounded-md shadow-2xl border-4 border-gray-300 text-gray-800 p-4 ">
+	<img src={image} alt="" className=" border-2 object-fill object-center w-full rounded-t-md h-72 bg-gray-300" />
+	<div className="flex flex-col justify-between p-6 space-y-8">
+		<div className="space-y-2">
+			<h2 className="text-3xl font-semibold tracking-wide">{title}</h2>
+			<p className="text-gray-800">Curabitur luctus erat nunc, sed ullamcorper erat vestibulum eget.</p>
+		</div>
+		<button type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md bg-indigo-600 text-gray-50">Course Details</button>
+	</div>
+</div>
+
     </div>
   );
 };
--- src/components/Pages/Courses/Courses.js
+++ src/components/Pages/Courses/Courses.js
@@ -16,27 +16,32 @@ useEffect(() => {
     const courseList = useLoaderData();
     
     return (
-       <div className='grid sm:grid-cols-4 lg:grid-cols-5 gap-5 bg-slate-400 px-10'>
+       <div className='grid md:grid-cols-5 gap-5 px-6'>
 
 
-<div className=''>
-   <h3 className='text-3xl font-semibold text-center mt-6'> Course Topics </h3>
+<div className='relative'>
+   <h3 className='text-3xl font-semibold text-center mt-4'> Course Topics </h3>
 
-    {
-        courseNames.map(course => <h3 className='text-2xl mt-4 bg-blue-900 text-center text-blue-300 list-none hover:bg-blue-500 hover:text-black' key={course.id}>
+  <div className='sticky top-10'>
+  {
+        courseNames.map(course => <h3 className='text-2xl mt-4 text-center hover:bg-blue-900 text-white tracking-wide bg-indigo-600' key={course.id}>
             <Link > 
             {course.title}
             </Link>
             </h3>)
     }
+  </div>
 </div>
 
 
-<div className='sm:col-span-3 lg:col-span-4'>
-<h3>This is the all cards area</h3>
+<div className='md:col-span-4 mt-8'>
+
+<div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 '>
 {   
 courseList.map(course => <CourseDetails key={course.id} course={course} /> )
 }  
+</div>
+
 </div>
       
     
